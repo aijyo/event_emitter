072edd63314ad2b398a6db45d69dcd1d715473eb
zhangzidan <Palim@ano-z.com>
2019-02-21 18:05:21 +0800

    add demo
    add demo

--- source.cpp
+++ source.cpp
@@ -1,14 +1,23 @@
 #include "event_emitter.h"
 
-void fnTest(int a, std::string str)
+void fnTest(int p1, std::string p2)
+{
+	return;
+}
+
+void fnTest1(int p1, std::string p2, double p3)
 {
 	return ;
+}
+void fnTest2(std::string p1, int p2, double p3 )
+{
+	return;
 }
 
 class test
 {
 public:
-	void fn(int a, std::string str)
+	void fn(int p1, std::string p2)
 	{
 		return;
 	}
@@ -25,14 +34,17 @@ int main(void)
 	};
 
 	int iParam = 1;
+	double dParam = 2.0;
 	std::string strParam = "z";
 
 	emitter.on("test", fn);
-	emitter.on("test1", &t, &test::fn);
-	emitter.on("test2", fnTest);
+	emitter.on("test", fnTest);
+	emitter.on("test", &t, &test::fn);
+	emitter.on("test1", fnTest1);
+	emitter.on("test2", fnTest2);
 	emitter.emit("test", iParam, strParam);
-	emitter.emit("test1", iParam, strParam);
-	emitter.emit("test2", iParam, strParam);
+	emitter.emit("test1", iParam, strParam, dParam);
+	emitter.emit("test2", strParam, iParam, dParam);
 
 	return 0;
 }
